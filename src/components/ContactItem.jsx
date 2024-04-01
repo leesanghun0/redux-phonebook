36cd7fe6f3761cb6253d6e4cc1d7c77c745bbea3
leesanghun <lsh93620@gmail.com>
2024-04-02 01:10:58 +0900

    ui 수정

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -6,11 +6,18 @@ const ContactItem =({item})=>{
     return(
     <Row className="contact-Card">
        <Col lg={1}>
-            <img width={50} src="https://i1.sndcdn.com/avatars-000373844735-9n06kq-t240x240.jpg" alt="" />
+            <img width={80} src="https://i1.sndcdn.com/avatars-000373844735-9n06kq-t240x240.jpg" alt="" />
        </Col>
-       <Col lg={10}>
-            <div>{item.name}</div>
-            <div>{item.phoneNumber}</div>
+       <Col lg={11} className="contact-Card-text">
+            <div className="user-info">
+                <div>{item.name}</div>
+                <div>{item.phoneNumber}</div>  
+            </div>
+            <div>
+                <i className="xi-call"></i>
+                <i className="xi-mail"></i>
+                <i className="xi-star"></i>
+            </div>
        </Col>
     </Row>
     )
